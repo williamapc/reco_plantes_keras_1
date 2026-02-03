1cc8fb5f07d444c24f9593f083696281aecbb0af
williamapc <w.h.b.a.p.c@gmail.com>
2026-02-03 13:11:27 +0100

    Initial commit

---
+++ webstreamlit/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import os
+
+st.write("Hello world! ")
+
+256*256
+
+"hello world" if False else "bye the world"
+
+pressed_button = st.button("Press Me")
+
+print(pressed_button)
+
+
+st.title("Hello World")
+st.header("Hello World")
+st.subheader("Hello World")
+st.markdown("###### _Hello World_")
+st.caption("small text")
+
+code_example = """
+def greet():
+    return "Hello World"
+"""
+
+st.code(code_example, language="python")
+
+st.divider()
+st.image(os.path.join(os.getcwd(), "static/", "0a5e9323-dbad-432d-ac58-d291718345d9___FREC_Scab 3417.JPG"))
